ab2bf30173871b124d00543b04227b7bc386023c
analyn pureza <analyn.c@hotmail.com>
2022-05-30 20:10:15 -0500

    Commit feature 3 changes.

--- map-mania-lite.js
+++ map-mania-lite.js
@@ -1,3 +1,16 @@
+// Notes relating to differences from other tutorials:
+// 1 - renamed "Map, Map, Map" to "myMapID, gMap, map"
+
+var gMap;
+
+// initMap is a callback function that is initiated as part of the Google Maps API call at the bottom
+// of the HTML file.
+function initMap() {
+    // Crate a new map and assign it to gMap
+    gMap = new google.maps.Map(document.getElementById('myMapID'), {
+        center: {lat: 41.878, lng:10}, zoom: 3});
+}
+
 function initApplication() {
     console.log('Map Mania Lite - Starting!');
 }
